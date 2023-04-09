2718932efc6c3c80448e4b082f033f0dd455c8ad
plumpsqrl9744 <rnwkddn1234@nate.com>
2023-04-09 23:27:09 +0900

    feat: 간단한 css추가

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -49,12 +49,14 @@ const SignIn = () => {
 
     return (
         <StSignInWrapper>
+            <StTitle>로그인</StTitle>
             <StInputWrapper>
                 <label>이메일</label>
                 <input 
                     data-testid="email-input"
                     onChange={onChangeHandler} 
                     name = "id"/>
+                <StValidation>@를 포함해주세요!</StValidation>
             </StInputWrapper>
             <StInputWrapper>
                 <label>비밀번호</label>
@@ -63,6 +65,7 @@ const SignIn = () => {
                     type = "password"
                     onChange={onChangeHandler} 
                     name = "password" />
+                    <StValidation>8자리 이상 입력해주세요!</StValidation>
             </StInputWrapper>
             <StInputWrapper>
                 <button 
@@ -78,6 +81,44 @@ const SignIn = () => {
 
 export default SignIn;
 
-const StSignInWrapper = styled.div``
-const StInputWrapper = styled.div``
+const StSignInWrapper = styled.div`
+    display : flex;
+    flex-direction : column;
+    align-items: center;
+    margin : 100px;
+`
+
+const StTitle = styled.div`
+    font-size : 30px;
+    font-weight : 600;
+`
+
+const StValidation = styled.div`
+    font-size : 12.5px;
+    color : blue;
+`
+const StInputWrapper = styled.div`
+    margin : 10px;
+    display : flex;
+    flex-direction : column;
+    label {
+        font-size : 20px;
+        margin-bottom : 5px;
+    }
+    input {
+        border : 1px solid gray;
+        width : 200px;
+        height : 20px;
+        margin-bottom : 5px;
+    }
+    button {
+        border : 0px;
+        width : 100px;
+        height : 30px;
+        border-radius : 10px;
+        background-color : blue;
+        font-size : 15px;
+        color : white;
+    }
+`
 
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -54,6 +54,7 @@ const SignUp = () => {
                     onChange={onChangeHandler} 
                     name = "id" 
                     />
+                    <StValidation>@를 포함해주세요!</StValidation>
             </StInputWrapper>
             <StInputWrapper>
                 <label>비밀번호</label>
@@ -63,6 +64,7 @@ const SignUp = () => {
                     onChange={onChangeHandler} 
                     name = "password" 
                     />
+                    <StValidation>8자리 이상 입력해주세요!</StValidation>
             </StInputWrapper>
             <StInputWrapper>
                 <button 
@@ -88,7 +90,30 @@ const StTitle = styled.div`
     font-weight : 600;
 `
 const StInputWrapper = styled.div`
-    align-items: center;
-    margin : 20px;
-    width : 400px;
+    margin : 10px;
+    display : flex;
+    flex-direction : column;
+    label {
+        font-size : 20px;
+        margin-bottom : 5px;
+    }
+    input {
+        border : 1px solid gray;
+        width : 200px;
+        height : 20px;
+        margin-bottom : 5px;
+    }
+    button {
+        border : 0px;
+        width : 100px;
+        height : 30px;
+        border-radius : 10px;
+        background-color : blue;
+        font-size : 15px;
+        color : white;
+    }
+`
+const StValidation = styled.div`
+    font-size : 12.5px;
+    color : blue;
 `
